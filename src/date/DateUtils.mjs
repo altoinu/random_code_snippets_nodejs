2d74d29339816e9f15504f45f3c3e2c7e60268bd
Kaoru Kawashima <8354804+altoinu@users.noreply.github.com>
2025-01-08 19:17:40 -0500

    date utils

---
+++ src/date/DateUtils.mjs
@@ -0,0 +1,48 @@
+/**
+ * Returns month in 01, 02, 03... 12
+ * @param {Date} date
+ * @returns Month in 01, 02, 03... 12
+ */
+export function getCurrentMonthInString(date) {
+  const month = date.getMonth() + 1;
+  return month.toString().padStart(2, "0");
+}
+
+/**
+ * Returns date in YYYY-MM-DD format
+ * @param {Date} date
+ * @returns date in YYYY-MM-DD format
+ */
+export function getDateString(date) {
+  return (
+    date.getFullYear().toString() +
+    "-" +
+    getCurrentMonthInString(date) +
+    "-" +
+    date.getDate().toString().padStart(2, "0")
+  );
+}
+
+function main() {
+  const january = new Date("2025-01-01T00:00:00");
+  const november = new Date("2025-11-11T00:00:00");
+
+  console.log(
+    "getCurrentMonthInString current time",
+    getCurrentMonthInString(new Date()),
+  );
+  console.log(
+    "getCurrentMonthInString january",
+    getCurrentMonthInString(january),
+  );
+  console.log(
+    "getCurrentMonthInString november",
+    getCurrentMonthInString(november),
+  );
+
+  console.log("getDateString current date", getDateString(new Date()));
+  console.log("getDateString january", getDateString(january));
+  console.log("getDateString november", getDateString(november));
+}
+
+main();
---
+++ src/date/DateUtils.test.mjs
@@ -0,0 +1,21 @@
+import { getCurrentMonthInString } from "./DateUtils.mjs";
+
+describe("DateUtils", () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, "log").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
+  describe("getCurrentMonthInString", () => {
+    it("returns single digit months with 0 prepended", () => {
+      expect(getCurrentMonthInString(new Date("2025-01-01T00:00:00"))).toBe(
+        "01",
+      );
+    });
+  });
+});
